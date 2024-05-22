5827d896804713e7c634a8427731ef9a95dc7482
rrnnlyyn77 <270176246@yoobeestudent.ac.nz>
2024-05-23 11:04:21 +1200

    created signupage.py

---
+++ SignUp.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request, flash, redirect, url_for
+import sqlite3 as sql
+
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('LandingPage.html')
+
+@app.route('/createNewAcc')
+def new_student():
+    return render_template("SignUp.html")
+
+@app.route('/createUser', methods=['GET','POST'])
+def contact():
+    if request.method=='POST':
+        try:
+            FName=request.form['FName']
+            LName=request.form['LName']
+            Email=request.form['Email']
+            Password=request.form['Password']
+            if not FName or LName or Email or Password:
+               
+                with sql.connect("gunita.db") as con:
+                    cur=con.cursor()
+                    cur.execute("insert into Users (FName, LName, Email, Password)values(?, ?, ?, ?)",(FName, LName, Email, Password))
+                    con.commit()
+                    msg="Account created successfully!"
+            else:
+                msg="We are unable to process your request, please try again."
+                con.rollback()
+        except:
+            msg="Operational error"
+            con.rollback()
+        finally:
+            return render_template("createUser.html", msg=msg)
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
+
+           
